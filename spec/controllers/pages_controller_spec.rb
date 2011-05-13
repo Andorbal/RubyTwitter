558a5e3cdc6a32de27b255e20185e09d09ae827c
Andrew Benz <andorbal@gmail.com>
2011-05-13 16:44:07 -0500

    Added the about view

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -16,4 +16,11 @@ describe PagesController do
     end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
 end
