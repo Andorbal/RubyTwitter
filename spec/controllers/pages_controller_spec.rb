938641d710ce12efef0f2e098a26e7a725e34279
Andrew Benz <andorbal@gmail.com>
2011-05-13 17:35:51 -0500

    Dome with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Ruby Twitter | Home")
     end
   end
 
@@ -14,12 +20,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Ruby Twitter | Contact")
+    end
   end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", content: "Ruby Twitter | About")
     end
   end
 
