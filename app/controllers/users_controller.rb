e17136cd01b7bb0dbaa224730136c64fe33292fe
Andrew Benz <andorbal@gmail.com>
2011-05-16 18:25:44 -0500

    Added error displayand styling for failed user creation attempts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,13 @@ class UsersController < ApplicationController
     @title = "Sign up"
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a sucessful save.
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
