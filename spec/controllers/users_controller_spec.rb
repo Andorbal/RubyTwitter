2ab7c99575c757761b8a9cfdf1c9c8349bc8abc6
Andrew Benz <andorbal@gmail.com>
2011-05-16 21:12:54 -0500

    Finished the profile link.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -120,6 +120,11 @@ describe UsersController do
         post :create, user: @attr
         flash[:success].should =~ /welcome to ruby twitter/i
       end
+
+      it "should sign the user in" do
+        post :create, user: @attr
+        controller.should be_signed_in
+      end
     end
   end
 
